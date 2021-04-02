cc08f0bf27c92567c065d6645ce86299e075e4cb
HP <12050113980@students.uin-suskaac.id>
2021-04-02 22:55:14 +0700

    roid fitrah utari

---
+++ src/ConditionalandLoops/conditionalstatement.java
@@ -0,0 +1,10 @@
+package ConditionalandLoops;
+
+public class conditionalstatement{
+    public static void main(String[] args) {
+        int x = 5;
+        if ( x==5 ){
+            System.out.println( "Yeah" );
+        }
+    }
+}
